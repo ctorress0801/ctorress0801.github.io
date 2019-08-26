2a2cb0f08f23230090eef6a315ca29d695d9a062
ctorress0801 <ctorress0801@gmail.com>
2019-08-26 15:56:47 -0500

    Aded info

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Sticky Menu
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickymenu()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function stickymenu() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
@@ -13,3 +13,17 @@ function myFunction() {
 }
 
 //Collapsible Mtnu Button
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
